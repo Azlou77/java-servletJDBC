5adfaf46719bc6a065a1f056d69065a61b308d27
Nguyen Louis <nguyenlouis172@gmail.com>
2025-05-07 16:48:35 +0200

    fix import jakarta servlet

---
+++ src/main/java/bonjour/Servlet.java
@@ -0,0 +1,13 @@
+package bonjour;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class Servlet extends HttpServlet {
+	public void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException{
+
+    }
+}
