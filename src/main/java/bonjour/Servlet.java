f2cf4452e3860dc2977e6645d7cecc6bd68faf51
Nguyen Louis <nguyenlouis172@gmail.com>
2025-05-13 22:19:12 +0200

    same with method post

--- src/main/java/bonjour/Servlet.java
+++ src/main/java/bonjour/Servlet.java
@@ -8,7 +8,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class Servlet extends HttpServlet {
-	public void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException{
+	public void doPost( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException{
 		response.setContentType("text/html");
 		response.setCharacterEncoding("UTF-8");
 		PrintWriter out = response.getWriter();
